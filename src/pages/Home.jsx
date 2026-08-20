796ea27fc78f24796cc7c8b889de31595f1a7bac
Dua Fatima <duafatimaoffical25@gmail.com>
2026-08-20 12:46:57 -0700

    home is coming bro

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from "../Components/Hero";
+import ProductSection from "../Components/ProductSection";
+
+function Home() {
+  return (
+    <main>
+      <Hero />
+      <ProductSection />
+    </main>
+  );
+}
+
+export default Home;
